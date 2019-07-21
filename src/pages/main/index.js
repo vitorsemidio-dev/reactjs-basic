2cd3cdce9cbea4320633e358b641c1fb2e44aa05
Vitor Emidio <vitorsemidio.96@gmail.com>
2019-07-21 12:40:37 -0300

    Navegacao entre paginas

--- src/pages/main/index.js
+++ src/pages/main/index.js
@@ -6,29 +6,59 @@ import './styles.css';
 export default class Main extends Component {
     state = {
         posts: [],
+        postInfo: {},
+        page: 1,
     }
     componentDidMount() {
         this.loadPosts();
     }
 
-    loadPosts = async () => {
+    loadPosts = async (page = 1) => {
         const response = await api.get('/posts');
         const { data } = response;
-        const posts = data.slice(0, 15);        
+        let total = data.length || 0;
+        const limit = 10, pages = total/limit;
+        const posts = data.slice(page-1, limit*page);
+        const postInfo = {
+            total, limit, page, pages,
+        }
+        console.log(postInfo);
 
-        this.setState({ posts })
+        this.setState({ posts, postInfo, page })
     }
+
+    prevPage = () => {
+        const { page } = this.state;
+        if (page === 1) return;
+        const pageNumber = page - 1;
+        this.loadPosts(pageNumber);
+    }
+
+    nextPage = () => {
+        const { page, postInfo } = this.state;
+
+        if (page === postInfo.pages) return;
+        
+        const pageNumber = page + 1;
+
+        this.loadPosts(pageNumber);
+    }
+
     render() {
-        const { posts } = this.state;
+        const { posts, page, postInfo } = this.state;
         return (
             <div className="post-list">
+                <div className="actions">
+                    <button disabled={page === 1} onClick={this.prevPage}>Anterior</button>
+                    <button disabled={page === postInfo.pages} onClick={this.nextPage}>Próximo</button>
+                </div>
                 {posts.map(post => (
                     <article key={post.id}>
                         <strong>{post.title}</strong>
                         <p>{post.body}</p>
                         <a href="www.google.com">Clique aqui</a>
                     </article>
-                ))}
+                ))}                
             </div>
         );
     }

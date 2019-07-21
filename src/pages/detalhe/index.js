7a6c8abf374c491ef262e88bb155448539bb4a9e
Vitor Emidio <vitorsemidio.96@gmail.com>
2019-07-21 13:28:53 -0300

    Navegacao para rota de detalhe

--- src/pages/detalhe/index.js
+++ src/pages/detalhe/index.js
@@ -1,9 +1,28 @@
 import React, { Component } from 'react';
+import api from '../../services/api';
 
+import './styles.css';
 export default class Detalhe extends Component {
+    state = {
+        post: {},
+    }
+
+    async componentDidMount() {
+        const { id } = this.props.match.params;
+
+        const response = await api.get(`/posts`)
+        const { data } = response;
+        const post = data[id - 1];
+        this.setState({ post })
+    }
     render() {
+        const { post } = this.state;
         return (
-            <h1>Teste</h1>
+            <div className="post-info">
+                <h1>{post.title}</h1>
+                <p>{post.body}</p>
+                <a href="www.github.com">Clique aqui</a>
+            </div>
         )
     }
 }
